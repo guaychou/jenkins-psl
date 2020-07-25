87370e396c37ed2f3ecde6d9f51efa6ee02d248c
guaychou <crossmajor99@gmail.com>
2020-07-25 15:57:11 +0700

    test4

--- src/com/workshop/main.groovy
+++ src/com/workshop/main.groovy
@@ -8,6 +8,8 @@ def main(script) {
    // Object initialization
    c = new Config()
    sprebuild = new prebuild()
+      sbuild = new build()
+
  
    // Pipeline specific variable get from injected env
    // Mandatory variable will be check at details & validation steps
@@ -17,7 +19,8 @@ def main(script) {
    def docker_user = ("${script.env.docker_user}" != "null") ? "${script.env.docker_user}" : ""
    def app_port = ("${script.env.app_port}" != "null") ? "${script.env.app_port}" : ""
    def pr_num = ("${script.env.pr_num}" != "null") ? "${script.env.pr_num}" : ""
- 
+ def docker_registry = ("${script.env.docker_registry}" != "null") ? "${script.env.docker_registry}" : "${c.default_docker_registry}"
+
    // Initialize docker tools
    def dockerTool = tool name: 'docker', type: 'dockerTool'
    // Pipeline object
@@ -28,7 +31,8 @@ def main(script) {
        docker_user,
        app_port,
        pr_num,
-       dockerTool
+       dockerTool,
+       docker_registry
    )
  
    ansiColor('xterm') {
@@ -41,9 +45,9 @@ def main(script) {
            sprebuild.checkoutBuildTest(p)
        }
  
-       //stage('Build & Push Image') {
-           // TODO: Call build & push image function
-       //}
+       stage('Build & Push Image') {
+            TODO: Call build & push image function
+       }
  
        //stage('Merge') {
            // TODO: Call merge function
